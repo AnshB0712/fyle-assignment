d09e946a0258eed1cc123a3902559af95fa659d7
Anshul Borde <89180266+AnshB0712@users.noreply.github.com>
2024-01-19 23:40:09 +0530

    Update index.js

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -5,7 +5,7 @@ const getUserInfo = async (req, res) => {
   try {
     const { data } = await axios.get(
       `https://api.github.com/users/${username}`,
-      {headers: {'User-Agent': 'anshb0712'}}
+      {headers: {'Authorization': 'token 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'}}
     );
     res.status(200).json({ ...data });
   } catch (error) {
@@ -18,7 +18,7 @@ const getUserRepos = async (req, res) => {
   try {
     const { data } = await axios.get(
       `https://api.github.com/users/${username}/repos`,
-      {headers: {'User-Agent': 'anshb0712'}}
+      {headers: {'Authorization': 'token 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'}}
     );
 
     const page = parseInt(req.query.page) || 1;
